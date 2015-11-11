81f13ecfec17e53654837089c8ef4566e27c6bad
Mayas Haddad <mayas_91@hotmail.com>
2015-11-11 12:16:29 +0100

    now deep checking if fields are provided

--- test/dataCheck.test.js
+++ test/dataCheck.test.js
@@ -6,7 +6,7 @@ module.exports = [
 	function() {
 		assert.strictEqual(
 			dataCheck.checkReqHasFields(
-					{}, []
+					{}, [], ''
 			).length,
 			0,
 			'no mandatory field provided'
@@ -16,7 +16,7 @@ module.exports = [
 	function() {
 		assert.strictEqual(
 			dataCheck.checkReqHasFields(
-					{firstname: 'John Doe'}, ['firstname']
+					{firstname: 'John Doe'}, ['firstname'], ''
 			).length,
 			0,
 			'firstname mandatory, firstname provided => must return [] meaning that no mandatory field\'s missing'
@@ -26,7 +26,7 @@ module.exports = [
 	function() {
 		assert.deepEqual(
 			dataCheck.checkReqHasFields(
-					{}, ['firstname']
+					{}, ['firstname'], ''
 			),
 			['firstname'],
 			'firstname mandatory, firstname not provided => must return [\'firstname\'] meaning that firstname mandatory field\'s missing'
@@ -36,7 +36,7 @@ module.exports = [
 	function() {
 		assert.deepEqual(
 			dataCheck.checkReqHasFields(
-					{firstname: 'John Doe'}, ['firstname', 'lastname']
+					{firstname: 'John Doe'}, ['firstname', 'lastname'], ''
 			),
 			['lastname'],
 			'lastname mandatory, lastname not provided => must return [\'lastname\'] meaning that lastname mandatory field\'s missing'
@@ -46,10 +46,40 @@ module.exports = [
 	function() {
 		assert.deepEqual(
 			dataCheck.checkReqHasFields(
-					{firstname: undefined}, ['firstname', 'lastname']
+					{firstname: undefined}, ['firstname', 'lastname'], ''
 			),
 			['firstname', 'lastname'],
 			'lastname mandatory, lastname not provided => must return [\'lastname\'] meaning that lastname mandatory field\'s missing'
+		)
+	},
+	
+	function() {
+		assert.deepEqual(
+			dataCheck.checkReqHasFields(
+					{firstname: 'John', address: '3 rue toto' }, ['firstname', { 'address': ['street']} ], ''
+			),
+			[ 'address.street' ],
+			'address is a json object, hence its field must be all provided '
+		)
+	},
+	
+	function() {
+		assert.deepEqual(
+			dataCheck.checkReqHasFields(
+					{firstname: 'John' }, ['firstname', { 'address': ['street']} ], ''
+			),
+			[ 'address' ],
+			'address is a json object, hence its field must be all provided '
+		)
+	},
+	
+	function() {
+		assert.deepEqual(
+			dataCheck.checkReqHasFields(
+					{firstname: 'John', address: { 'street': {'oldName': 'honlulu'} } }, ['firstname', { 'address': [ { 'street': ['oldName', 'newName'] } ] } ], ''
+			),
+			[ 'address.street.newName' ],
+			'address is a json object, hence its field must be all provided '
 		)
 	}
 ]
